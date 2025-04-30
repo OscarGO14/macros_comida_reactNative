806f24b1599ff93860b90e75184ace86e838b620
Oscargo14 <ogo53140@gmail.com>
2025-04-30 20:02:04 +0200

    Setup NativeWind with Expo Router and configure project structure

--- app/ingredients.tsx
+++ app/ingredients.tsx
@@ -1,8 +1,9 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
-import useCollection from "@/hooks/useCollection";
-import Collections from "@/types/collections";
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import useCollection from '@/hooks/useCollection';
+import Collections from '@/types/collections';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function IngredientsScreen() {
   const {
@@ -13,7 +14,7 @@ export default function IngredientsScreen() {
 
   if (ingredientsLoading) {
     return (
-      <View style={styles.container}>
+      <View>
         <Text>Loading...</Text>
       </View>
     );
@@ -21,7 +22,7 @@ export default function IngredientsScreen() {
 
   if (ingredientsError) {
     return (
-      <View style={styles.container}>
+      <View>
         <Text>Error loading ingredients: {ingredientsError.message}</Text>
       </View>
     );
@@ -29,38 +30,31 @@ export default function IngredientsScreen() {
 
   if (!ingredientsData) {
     return (
-      <View style={styles.container}>
+      <View>
         <Text>No data available.</Text>
       </View>
     );
   }
 
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <View>
-        <Text>Ingredients List:</Text>
+    <SafeAreaView>
+      <View className="flex">
+        <StatusBar style="auto" />
+        <View>
+          <Text>Ingredients List:</Text>
+        </View>
+        <ScrollView className="">
+          {ingredientsData.length > 0 ? (
+            ingredientsData.map((ingredient) => (
+              <Text key={ingredient.id}>
+                {ingredient.name} - {ingredient.calories} kcal
+              </Text>
+            ))
+          ) : (
+            <Text>No ingredients found.</Text>
+          )}
+        </ScrollView>
       </View>
-      <ScrollView>
-        {ingredientsData.length > 0 ? (
-          ingredientsData.map((ingredient) => (
-            <Text key={ingredient.id}>
-              {ingredient.name} - {ingredient.calories} kcal
-            </Text>
-          ))
-        ) : (
-          <Text>No ingredients found.</Text>
-        )}
-      </ScrollView>
-    </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 50,
-    paddingHorizontal: 20,
-    backgroundColor: "#fff",
-  },
-});
